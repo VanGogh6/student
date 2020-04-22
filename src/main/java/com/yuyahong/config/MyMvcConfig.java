3be682bba68d85f393a135bab65a7cb2981a8aa5
van <520200wf@163.com>
2020-04-22 12:01:50 +0800

    修改拦截器放行静态资源无效，改为交给sping管理

--- src/main/java/com/yuyahong/config/MyMvcConfig.java
+++ src/main/java/com/yuyahong/config/MyMvcConfig.java
@@ -1,9 +1,8 @@
 package com.yuyahong.config;
 
 import com.yuyahong.interceptor.LoginHandlerInterceptor;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,28 +14,28 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * @author yuyahong
  * @date 2020/4/21 0021 20:22
  */
-@Configuration
+@Component
 public class MyMvcConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LoginHandlerInterceptor()).addPathPatterns("/**")//拦截所有请求
-                //放行的请求
+                //放行的请求  , "**.css", "**.js", "**.jpg", "**.eot", "**.svg", "**.ttf", "**.woff", "**.woff2"
                 .excludePathPatterns("/toLogin", "/login");
     }
 
-    /**
-     * 放行静态资源
-     *
-     * @param registry
-     */
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
-        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/js/");
-        registry.addResourceHandler("/css/**").addResourceLocations("classpath:/css/");
-        registry.addResourceHandler("/images/**").addResourceLocations("classpath:/images/");
-        registry.addResourceHandler("/images/**").addResourceLocations("classpath:/fonts/");
-    }
+//    /**
+//     * 放行静态资源
+//     *
+//     * @param registry
+//     */
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+//        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/js/");
+//        registry.addResourceHandler("/css/**").addResourceLocations("classpath:/css/");
+//        registry.addResourceHandler("/images/**").addResourceLocations("classpath:/images/");
+//        registry.addResourceHandler("/images/**").addResourceLocations("classpath:/fonts/");
+//    }
 
     /**
      * 视图
